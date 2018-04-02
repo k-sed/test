04ce4a15ed2dee7ad7c5b554de4a5c961e600002
Konstantin Sedanov <konstantin.sedanov@rosberry.com>
2018-04-02 13:41:28 +0600

    test fw

---
+++ backend/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.New()
+	router.Use(
+		gin.Recovery(),
+	)
+
+	api := router.Group("/api")
+	{
+		api.GET("/me", func(c *gin.Context) {
+			c.String(http.StatusOK, "Hello world!")
+		})
+	}
+
+	router.Run(":6060")
+}
